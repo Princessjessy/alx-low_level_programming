a2745fd33c4e6e1564bcd4e6195f3b74cfdfc4aa
Princessjessy <ebubeprincess590@gmail.com>
2023-02-21 18:34:20 -0800

    timertable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,42 +1,39 @@
 #include "main.h"
 
+
 /**
-* times_table - prints the 9 times table, starting with 0
-*/
+ * times_table - Prints the 9 times table
+ *
+ * Return: no return
+ */
 
 void times_table(void)
+{
+	int a, b, op;
+
+for (a = 0; a <= 9; a++)
+{
+_putchar(48);
+
+for (b = 1; b <= 9; b++)
 
 {
-
-	int x, y, z, u, d;
-	for (x = 0; x <= 9; x++)
-
-	{
-	for (i = 0; i < 9; y++)
-	{
-	z = x * y;
-	if (z > 9)
-	{
-	u + z % 10;
-	d = (z - u) / 10;
-	_putchar(44);
-	_putchar(32);
-	_putchar(d + '0');
-	_putchar(u + '0');
-	}
-	else
-	{
-	if (y != 0)		
-	{		
-	_putchar(44);
-	_putchar(32);
-	_putchar(32);
-	}
-	_putchar(z + '0');
-	}
-	}			
-	_putchar('\n');
-	}
+op = a * b;
+_putchar(44);
+_putchar(32);
+if (op <= 9)
+{
+_putchar(32);
+_putchar(op + 48);
+}
+else
+{
+_putchar((op / 10) + 48);
+_putchar((op % 10) + 48);
+}
+}
+_putchar('\n');
 }
 
+}
 
