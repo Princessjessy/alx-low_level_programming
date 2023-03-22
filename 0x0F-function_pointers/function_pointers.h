15389bc2f5cd3d0919ab0eef7da927b4e16195fa
princessjessy <ebubeprincess590@gmail.com>
2023-03-22 14:34:44 -0700

    3-op_functions

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h>
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
@@ -16,5 +19,5 @@ typedef enum bool
 	FALSE = 0
 } bool;
 
-#endif /* FUNCTION_POINTERS_H */
+#endif 
 
