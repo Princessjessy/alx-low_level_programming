4e962aef1295a30f2bbcc7b8350628dc7cd5e615
princessjessy <ebubeprincess590@gmail.com>
2023-04-10 14:24:01 -0700

    file l/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,55 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
  *
- * Return: numbers of letters printed. It fails, returns 0.
+ * Return: actual number of letters it could read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int i;
-	ssize_t rd,wt;
 	char *buff;
+	ssize_t wt, rd;
 
 	if (!filename)
 		return (0);
-
 	i = open(filename, O_RDONLY);
-
 	if (i == -1)
+	{
+		close(i);
 		return (0);
+	}
 
-	buff = malloc(sizeof(char) * (letters));
+	buff = malloc(sizeof(char) * letters);
 	if (!buff)
+	{
+		close(i);
 		return (0);
+	}
 
-	rd = read(i, buff, letters);
-	wt = write(STDOUT_FILENO, buff, rd);
+	wt = read(i, buff, letters);
 
+	if (wt == -1)
+	{
+		close(i);
+		free(buff);
+		return (0);
+	}
+
+	rd = write(STDOUT_FILENO, buff, wt);
+
+	if (rd == -1)
+	{
+		close(i);
+		free(buff);
+		return (0);
+	}
 	close(i);
-
-	free(buff);
-
 	return (wt);
 }
