86ccb92bd5a7be6407631cd30135fc918b1dacef
princessjessy <ebubeprincess590@gmail.com>
2023-04-10 14:39:19 -0700

    file l/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 char *create_buffer(char *file);
-void close_file(int d);
+void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
@@ -31,15 +31,15 @@ char *create_buffer(char *file)
  * close_file - Closes file descriptors.
  * @fd: The file descriptor to be closed.
  */
-void close_file(int d)
+void close_file(int fd)
 {
 	int i;
 
-	i = close(d);
+	i = close(fd);
 
 	if (i == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", d);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -58,7 +58,7 @@ void close_file(int d)
  */
 int main(int argc, char *argv[])
 {
-	int from, text, rd, wt;
+	int from, text, w, r;
 	char *buffer;
 
 	if (argc != 3)
@@ -69,11 +69,11 @@ int main(int argc, char *argv[])
 
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
-	rd = read(from, buffer, 1024);
+	w = read(from, buffer, 1024);
 	text = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from == -1 || rd == -1)
+		if (from == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
@@ -81,8 +81,8 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 
-		wt = write(text, buffer, rd);
-		if (text == -1 || wt == -1)
+		r = write(text, buffer, w);
+		if (text == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
@@ -90,10 +90,10 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 
-		rd = read(from, buffer, 1024);
+		w = read(from, buffer, 1024);
 		text = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (rd > 0);
+	} while (w > 0);
 
 	free(buffer);
 	close_file(from);
